28241a79b9609f2cafd0ad8ec098969f94388246
MuhammadUmerMujtaba <145785569+MuhammadUmerMujtaba@users.noreply.github.com>
2024-07-11 15:22:50 +0500

    My receipt, delivery page done

--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_receipt.dart';
 
 class DeliveryProgressPage extends StatelessWidget {
   const DeliveryProgressPage({Key? key}) : super(key: key);
@@ -7,7 +8,102 @@ class DeliveryProgressPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Delivery is in progress...'),
+        title: Text('Delivery is in progress..'),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_rounded),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          MyReceipt(),
+        ],
+      ),
+      bottomNavigationBar: _buildBottomNavigationBar(context),
+    );
+  }
+
+  Widget _buildBottomNavigationBar(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.secondary,
+        borderRadius: BorderRadius.circular(40),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surface,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: Icon(Icons.person),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Mitch Koko',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                Text(
+                  "Driver",
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                )
+              ],
+            ),
+            Spacer(),
+            Row(
+              children: [
+                //message button
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.message,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+                SizedBox(width: 10),
+                //call button
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.surface,
+                    shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.call,
+                      color: Colors.green,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
